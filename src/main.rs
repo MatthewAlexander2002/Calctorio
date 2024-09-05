40f54a494617fa13a76160f907c18aee82dd5e48
MatthewAlexander2002 <matthewalexander009@gmail.com>
2024-09-05 12:42:02 +1000

    setting up read in and removing white space

--- src/main.rs
+++ src/main.rs
@@ -17,4 +17,24 @@ fn main() {
     file.read_to_string(&mut contents).expect("something went wrong reading the file");
 
     println!("With text: \n{}", contents);
+
+    // let mut token_start = 0;
+    let mut current_token = String::new();
+    let mut found_tokens: Vec<String> = vec![];
+
+    for (i, c) in contents.chars().enumerate(){
+
+        // print!("{:?}, ", i);
+        // println!("{:?}", c);
+
+        if c == '\n' || c == '\t' || c == ' ' {
+            if current_token != String::new() {
+                println!("{:?}", current_token);
+                found_tokens.push(current_token);
+                current_token = String::new();
+            }
+        } else {
+            current_token.push(c);
+        }
+    }
 }
