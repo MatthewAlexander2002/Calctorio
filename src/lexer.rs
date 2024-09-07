b19aa1cec1b4e6c61f8efb3154e948c1a185c62a
MatthewAlexander2002 <matthewalexander009@gmail.com>
2024-09-07 12:22:02 +1000

    added enum, still needs extra functionality

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,6 +2,46 @@ use std::vec;
 use std::fs::File;
 use std::io::prelude::*;
 
+enum Tokens {
+    Int(i32), // int
+    Double(i32, u32), // double
+    Const, // const
+    If, // if
+    For, // for
+    While, // while
+    Print, // print
+    Size, // size
+    ToINT(i32, u32), // toINT
+    ToDouble(i32), // toDouble
+    Break, // break
+    Continue, // continue
+    Return(Some), // return
+    And, // &&
+    Or, // ||
+    GreaterThan, // >
+    LessThan, // <
+    GreaterThanEqual, // >=
+    LessThanEqual, // <=
+    NotEqual, // <>
+    Equal, // ==
+    Assignment, // =
+    Plus, // +
+    Minus, // -
+    Times, // * 
+    Divide, // /
+    Modulo, // %
+    BracketL, // (
+    BracketR, // )
+    CurlyBracketL, // {
+    CurlyBracketR, // }
+    SquareBracketL, // [
+    SquareBracketR, // ]
+    Semi, // ;
+    CommentL, // /* 
+    CommentR, // */
+}
+
+
 pub fn lexer(file_loc: &str) -> Vec<String> {
     let mut file = File::open(file_loc).expect("file not found");
 
@@ -10,14 +50,15 @@ pub fn lexer(file_loc: &str) -> Vec<String> {
 
     println!("With text: \n{}", contents);
 
+    //these have been replaced by the enum but until the enum is fin they will be left here 
     //probably need a better name for symbols and scope
-    let legal_symbols: Vec<&str> = vec!["int", "double", "const", "if", "for", "print", "size", "toINT", "toFloat", "break", "return"];
-    let legal_binary_opaterators: Vec<&str> = vec!["&&", "||", "<", ">", "<=", ">=", "<>", "=="];
-    let legal_operator: Vec<&str> = vec!["=", "+", "-", "*", "/", "%"];
-    let legal_scope: Vec<&str> = vec!["(", ")", "{", "}", "[", "]", ";", "/*", "*/"];
+    // let legal_symbols: Vec<&str> = vec!["int", "double", "const", "if", "for", "print", "size", "toINT", "toDouble", "break", "return"];
+    // let legal_binary_opaterators: Vec<&str> = vec!["&&", "||", "<", ">", "<=", ">=", "<>", "=="];
+    // let legal_operator: Vec<&str> = vec!["=", "+", "-", "*", "/", "%"];
+    // let legal_scope: Vec<&str> = vec!["(", ")", "{", "}", "[", "]", ";", "/*", "*/"];
     // let mut token_start = 0;
     let mut current_token = String::new();
-    let mut found_tokens: Vec<String> = vec![];
+    let mut found_tokens: Vec<Tokens> = vec![];
 
     for (i, c) in contents.chars().enumerate(){
 
