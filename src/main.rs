7da781f7858966ea4230a4cdb6a584caf4080604
MatthewAlexander2002 <matthewalexander009@gmail.com>
2024-11-24 19:55:05 +1100

    added functionally to read all files from directory and run them as tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(warnings)]
 
 use parser::{TreeNode, Symbol, NonTerminal};
+use std::fs;
+use std::path::Path;
 mod lexer;
 mod parser;
 
@@ -13,14 +15,33 @@ fn main() {
     // println!("{:?}", filename);
     let filename = "/home/matthew/Documents/UNI/Sem 6/SDL/Calctorio/TestSuite/AtomicTests/EOF";
     let tokens = lexer::lexer(filename);
-
-    // parser::parser(&tokens);
-
-    match parser::parser(&tokens) {
-        Ok(tree) => println!("Parse successful: {:?}", tree),
-        Err(e) => println!("Parse error: {}", e),
+    fn find_test_files(path: &Path) -> Vec<String> {
+        let mut files = Vec::new();
+        if path.is_dir() {
+            for entry in fs::read_dir(path).unwrap() {
+                let entry = entry.unwrap();
+                let path = entry.path();
+                if path.is_dir() {
+                    files.extend(find_test_files(&path));
+                } else {
+                    files.push(path.to_str().unwrap().to_string());
+                }
+            }
+        }
+        files
     }
-    
+
+    let test_dir = Path::new("/home/matthew/Documents/UNI/Sem 6/SDL/Calctorio/TestSuite");
+    let test_files = find_test_files(test_dir);
+
+    for file in test_files {
+        println!("\nTesting file: {}", file);
+        let tokens = lexer::lexer(&file);
+        match parser::parser(&tokens) {
+            Ok(tree) => println!("Parse successful: {:?}", tree),
+            Err(e) => println!("Parse error: {}", e),
+        }
+    } 
 }
 
 // fn print_tree(node: &parser::TreeNode, depth: usize) {
