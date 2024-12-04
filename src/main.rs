9e9ed7dd87197d7f9288282e956722ecb628c29f
MatthewAlexander2002 <matthewalexander009@gmail.com>
2024-12-04 11:08:17 +1100

    many operators can work

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() {
     //     println!("\n--------------------");
     // }
 
-    let file = "/home/matthew/Documents/UNI/Sem 6/SDL/Calctorio/TestSuite/InterpreterTests/Modulo";
+    let file = "/home/matthew/Documents/UNI/Sem 6/SDL/Calctorio/TestSuite/InterpreterTests/ManyOps";
     let tokens = lexer::lexer(&file);
         match parser::parser(&tokens) {
             Ok(tree) => {
